b441216f25e6788f720189a584971d936e3d688f
Sebastian Aguiar <sebastianaguiar9511@gmail.com>
2026-04-14 14:57:24 -0500

    client2

--- temp_monitor_client.py
+++ temp_monitor_client.py
@@ -10,15 +10,17 @@ def main():
     filename = input("Ingrese el nombre del archivo:\n")
     # TODO: Abrir el archivo y leer el numero de lecturas n
     with open(filename, "r") as f:
-        lineas = (f.readline().strip())
+        line = int(f.readline().strip())
     # TODO: Crear el monitor usando temp_monitor.init(n)
-        monitor = {temp_monitor.init()}
+        monitor = {temp_monitor.init(line)}
     # TODO: Leer las n temperaturas y agregarlas con temp_monitor.add_reading()
-    
+        for line in f:
+            temperature = float(line.strip())
+            temp_monitor.add_reading(temperature)
     # TODO: Imprimir la racha creciente mas larga
     #       usando temp_monitor.longest_rising_streak()
-    
-    pass
+        streak =temp_monitor.longest_rising_streak(line)
+    print(streak)
 
 
 if __name__ == "__main__":
